2cb124a690514b50b84a3c270089e2967820889b
sorkwhwo <sorkwhwo@naver.com>
2022-04-24 11:22:25 +0900

    wow

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class _AppState extends State<App> {
       onWillPop: () async {
         return false;
       },
+      //dls
       child: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (context, snapshot) {
